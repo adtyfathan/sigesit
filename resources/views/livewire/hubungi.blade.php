54e50358eb1a1ce8692aecd52ad06727075fe38b
adtyfathan <adtyfathan04@gmail.com>
2025-08-01 08:28:36 +0700

    fix some design

--- resources/views/livewire/hubungi.blade.php
+++ resources/views/livewire/hubungi.blade.php
@@ -15,8 +15,10 @@
                 <h3 class="text-2xl font-bold text-gray-900 mb-6">Informasi Kontak</h3>
                 <div class="space-y-6">
                     <p class="flex items-center text-gray-600">
-                        <svg class="flex-shrink-0 h-6 w-6 text-indigo-600 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 12.42m0 0-.282.654L11.90 12.42m-1.043-.217L11.90 12.42m0 0-2.824-.654L8.148 21.672m4.704-9.252V21.672M12 4.5V9m7.21-6.72A1.5 1.5 0 0 1 20.72 4.5L12 9 3.28 4.5a1.5 1.5 0 0 1 1.042-2.22A1.5 1.5 0 0 1 5.92 3L12 7.218 18.08 3a1.5 1.5 0 0 1 1.93-.33Z" />
+                        <svg class="flex-shrink-0 h-6 w-6 text-indigo-600 mr-3" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                            viewBox="0 0 24 24" stroke="none">
+                            <path
+                                d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
                         </svg>
                         <span>Jl. Raya Jakarta - Bogor KM. 46, Kabupaten Bogor, 16911, Jawa Barat, Indonesia</span>
                     </p>
@@ -28,7 +30,8 @@
                     </p>
                     <p class="flex items-center text-gray-600">
                         <svg class="flex-shrink-0 h-6 w-6 text-indigo-600 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.332L19.24 15.3l-2.828-2.828-1.474 1.474L15 17.25l-2.757-2.757c-2.404-2.405-3.844-5.385-4.509-8.423L6.75 6.75l1.5-1.5-.75-.75H3.75A2.25 2.25 0 0 0 1.5 3v.75Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 5a2 2 0 0 1 2-2h3.28a1 1 0 0 1 .948.684l1.498 4.493a1 1 0 0 1-.502 1.21l-2.257 1.13a11.042 11.042 0 0 0 5.516 5.516l1.13-2.257a1 1 0 0 1 1.21-.502l4.493 1.498a1 1 0 0 1 .684.949V19a2 2 0 0 1-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
                         <span>021-8753155</span>
                     </p>
